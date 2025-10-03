af1f66fb30d79f6b968169e086c7750d1aeae7bc
hsyeun <sooy0614@naver.com>
2025-10-04 00:43:49 +0900

    시간 조회 수정

--- _plugins/git_date.rb
+++ _plugins/git_date.rb
@@ -6,29 +6,32 @@ module Jekyll
 
     def generate(site)
       site.posts.docs.each do |post|
-        relative_path = post.path.sub(site.source + '/', '')
+        # Git 로그 확인용 절대 경로
+        absolute_path = File.expand_path(post.path, site.source)
 
-        # 해당 파일의 최초 추가 커밋 시간들 (디렉토리 이동 포함)
-        git_times = `git log --follow --diff-filter=A --format="%ci" -- "#{relative_path}"`.strip.split("\n")
+        # 최초 추가된 커밋 시간 가져오기 (--diff-filter=A = 추가된 기록만)
+        git_times = `git log --follow --diff-filter=A --format="%ci" -- "#{absolute_path}"`.strip.split("\n")
 
         if !git_times.empty?
+          # 최초 커밋 시간 = 맨 마지막 기록
           created_time = Time.parse(git_times.last)
-
-          # post.date 와 내부 캐시(@date) 모두 덮어쓰기
-          post.data['date'] = created_time
-          post.instance_variable_set(:@date, created_time)
-
         else
-          # Git 기록 없을 경우 → 파일 생성시간 fallback
-          begin
-            created_time = File.birthtime(post.path)
-          rescue
-            created_time = File.mtime(post.path)
-          end
-
-          post.data['date'] = created_time
-          post.instance_variable_set(:@date, created_time)
+          # Git 기록 없을 경우 → 파일 생성/수정 시간 사용
+          created_time =
+            if File.respond_to?(:birthtime)
+              begin
+                File.birthtime(post.path)
+              rescue
+                File.mtime(post.path)
+              end
+            else
+              File.mtime(post.path)
+            end
         end
+
+        # post.date 와 내부 캐시(@date) 모두 덮어쓰기
+        post.data['date'] = created_time
+        post.instance_variable_set(:@date, created_time)
       end
     end
   end
